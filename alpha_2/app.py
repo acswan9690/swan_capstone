31aa835d503d130797c0b6a2943949ee36a2fb14
Adam Swan <acswan9690@gmail.com>
2021-10-22 16:31:30 -0500

    bug fix

--- alpha_2/app.py
+++ alpha_2/app.py
@@ -5,12 +5,12 @@ from app_pages import main_app, water_app
 
 app = MultiApp()
 
+app.add_app("Vegetable Predictor", main_app.app)
+app.add_app("Watering Estimate", water_app.app)
+
 st.sidebar.title("About")
 st.sidebar.info("This app was created by Adam Swan - October 2021")
 st.sidebar.title("Sources")
 st.sidebar.info("1. https://gardening.cals.cornell.edu/ \n 2. https://www.almanac.com/")
-
-app.add_app("Vegetable Predictor", main_app.app)
-app.add_app("Watering Estimate", water_app.app)
 
 app.run()
